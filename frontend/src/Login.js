b78755c039ce8b687e124b543b09b7be2ad1967f
stardandev <oliverstylenow@gmail.com>
2021-07-05 05:32:31 -0700

    add error handle

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,6 +5,7 @@ const Login = () => {
     const loginMetamask = async () => {
         let web3;
         let accounts;
+        
         if (window.ethereum) {
             accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             web3 = new Web3(window.ethereum);
@@ -15,20 +16,21 @@ const Login = () => {
                 const nonce = "Signature with Nonce" + res.data;
                 const signature = await web3.eth.personal.sign(nonce, accounts[0]);
 
-                const auth = await axios.post(process.env.REACT_APP_URL + "/auth", {
+                axios.post(process.env.REACT_APP_URL + "/auth", {
                     address: accounts[0],
                     nonce: nonce,
                     signature
-                });
-    
-                if (auth.data === "Hello World!") {
-                    alert("Login success");
-                } else {
+                }).then(auth => {
+                    if (auth.data === "Hello World!") {
+                        alert("Login success");
+                    } else {
+                        alert("Login failed")
+                    }
+                })
+                .catch(() => {
                     alert("Login failed")
-                }
+                });
             });
-
-            
         }
     }
 
